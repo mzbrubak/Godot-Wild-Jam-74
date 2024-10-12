5b7a6ba838eb02e87134aaaf289bcad6bf1b71ad
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-10-12 13:26:39 -0700

    Basic menu screen
    The exit button works and there isn't really art yet, but it's a start

---
+++ MainMenu.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+func _on_new_game_button_pressed():
+	#Load first scene
+	pass # Replace with function body.
+
+
+func _on_continue_button_pressed():
+	#Load scene based on save data
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	#Transition to options screen
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	#close the game
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
