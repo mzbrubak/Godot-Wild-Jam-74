c499303aafdc56ab337d8fd4d1da1ab8e3932c2a
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-10-12 21:51:25 -0700

    Resolution setting does something
    I can consider more options for changing the screen size later; this at least gives a baseline

--- options_from_title.gd
+++ options_from_title.gd
@@ -5,19 +5,23 @@ func _ready():
 	$"VBoxContainer/Music Volume".value=Settings.musicvolume;
 	$"VBoxContainer/SFX Volume".value=Settings.sfxvolume;
 	$"VBoxContainer/Tutorial Dialogues".button_pressed=Settings.tutorialmessages;
+	$"VBoxContainer/ResolutionOption/ResolutionDropdown".selected=Settings.resolutionid;
 
 func _on_back_button_pressed():
 	Settings.savechanges();
 	get_tree().change_scene_to_file("res://main_menu.tscn");
 
 func _on_master_volume_value_changed(value):
-	Settings.mastervolume=value;
+	Settings.set_mastervolume(value);
 
 func _on_music_volume_value_changed(value):
-	Settings.musicvolume=value;
+	Settings.set_musicvolume(value);
 
 func _on_sfx_volume_value_changed(value):
-	Settings.sfxvolume=value;
+	Settings.set_sfxvolume(value);
 
 func _on_tutorial_dialogues_toggled(toggled_on):
-	Settings.tutorialmessages=toggled_on;
+	Settings.set_tutorialmessages(toggled_on);
+
+func _on_resolution_dropdown_item_selected(index):
+	Settings.set_resolution(index);
--- settings.gd
+++ settings.gd
@@ -1,5 +1,5 @@
 extends Node
-var resolution=0;#this is going to be retooled at some point
+var resolutionid=0;#this is going to be retooled at some point
 var mastervolume:float=50;
 var musicvolume:float=50;
 var sfxvolume:float=50;
@@ -11,17 +11,35 @@ func _ready():
 	if err!=OK:
 		#guess you're stuck with the built-in defaults!  congrats on first boot-up
 		return
-	resolution=config.get_value("Video","resolution");
+	resolutionid=config.get_value("Video","resolutionid");
 	mastervolume=config.get_value("Audio","mastervolume",50);
 	musicvolume=config.get_value("Audio","musicvolume",50);
 	sfxvolume=config.get_value("Audio","sfxvolume",50);
 	tutorialmessages=config.get_value("Gameplay","tutorialmessages",true);
+	set_resolution(resolutionid);
 
 func savechanges():
 	var config=ConfigFile.new()
-	config.set_value("Video","resolution",resolution);
+	config.set_value("Video","resolutionid",resolutionid);
 	config.set_value("Audio","mastervolume",mastervolume);
 	config.set_value("Audio","musicvolume",musicvolume);
 	config.set_value("Audio","sfxvolume",sfxvolume);
 	config.set_value("Gameplay","tutorialmessages",tutorialmessages);
 	config.save("user://settings.ini");
+
+func set_resolution(index):
+	resolutionid=index;
+	var resolution=Vector2i(640*(1+index),360*(1+index));
+	DisplayServer.window_set_size(resolution);
+	
+func set_mastervolume(value):
+	mastervolume=value;
+	
+func set_musicvolume(value):
+	musicvolume=value;
+
+func set_sfxvolume(value):
+	sfxvolume=value;
+	
+func set_tutorialmessages(toggled):
+	tutorialmessages=toggled;
