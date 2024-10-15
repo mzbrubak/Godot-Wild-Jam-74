2694e747528c45a19fe47237f2f12376f0c84726
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-10-14 21:57:41 -0700

    some trash assets
    Hopefully I find the pen for my tablet soon, drawing with mouse sucks.  Also this is just an excuse, I'm bad at art (ex: blurry pfp)

--- SaveData.gd
+++ SaveData.gd
@@ -2,7 +2,7 @@ extends Node
 var machines_repaired=[false,false,false,false,false]
 var savegame_exists:bool = false
 
-func _ready():
+func loadsavedgame():
 	var config=ConfigFile.new();
 	var err = config.load("user://savedata.ini");
 	if err!=OK:
@@ -11,3 +11,7 @@ func _ready():
 		return
 	machines_repaired=config.get_value("","Machines Repaired", machines_repaired)
 	
+func savegame():
+	var config=ConfigFile.new()
+	config.set_value("","Machines Repaired",machines_repaired);
+	config.save("user://savedata.ini");
